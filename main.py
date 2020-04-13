9cd76d45841a096f10f3be3268b4953c7fda3774
sam-karis <samkaris75@gmail.com>
2020-04-13 22:11:00 +0300

    - add hosted api url  - add xml and json response

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from xml.etree.ElementTree import Element, tostring
+
 from fastapi import FastAPI
 from pydantic import BaseModel
+from starlette.requests import Request
+from xmljson import badgerfish as bf
 
 from src.estimator import estimator
 
@@ -23,6 +27,16 @@ class RequestData(BaseModel):
 
 
 @app.post('/api/v1/on-covid-19')
-def estimate_covid_19(body: RequestData):
+@app.post('/api/v1/on-covid-19/json')
+@app.post('/api/v1/on-covid-19/xml')
+def estimate_covid_19(request: Request, body: RequestData):
     result = estimator(body.dict())
+    if request.url.path.endswith('xml'):
+        result = bf.etree(result, root=Element('html'))
+        result = tostring(result)
     return result
+
+
+@app.get('/api/v1/on-covid-19/logs')
+def endpoint_logs():
+    pass
